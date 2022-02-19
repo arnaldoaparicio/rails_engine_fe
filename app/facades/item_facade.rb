83bdff2ff931d3060a81e31488234ef21f439572
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-19 15:07:50 -0500

    Fix spacing error

--- app/facades/item_facade.rb
+++ app/facades/item_facade.rb
@@ -1,6 +1,6 @@
-class ItemFacade 
+class ItemFacade
 
-    def self.item_show(item_id)
-        Item.new(ItemService.item_info(item_id))
-    end
+  def self.item_show(item_id)
+    Item.new(ItemService.item_info(item_id))
+  end
 end
