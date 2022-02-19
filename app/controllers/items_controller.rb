dfddb90e5cfca41853d990a6caf52ce13f6eb4e1
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-19 13:34:35 -0500

    Refactor show method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,5 @@
 class ItemsController < ApplicationController
-  def connection
-    Faraday.new(url: 'http://localhost:3000/')
-  end
-
   def show
-
-    response = connection.get("api/v1/items/#{params[:item_id]}")
-    @item = JSON.parse(response.body, symbolize_names: true)
+    @item = ItemFacade.item_show(params[:item_id])
   end
-
 end
